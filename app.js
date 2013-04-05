2b84345b019eb67cd92b7008ac033d61650aba96
wbyoko <wbagayoko@gmail.com>
2013-04-05 02:01:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,37 @@
     dummyStr,
     dummyVar,
     interval,
-    i, x, s;
+    i, x, s,
+    pfx;
 
+  pfx = ["webkit", "moz", "MS", "o", ""];
+  function PrefixedEvent(elem, type, callback) {
+    for (var p = 0; p < pfx.length; p++) {
+		  if (!pfx[p]) type = type.toLowerCase();
+		  elem.addEventListener(pfx[p]+type, callback, false);
+	  }
+  }
+  
   dummyStr = 'flash bounce shake tada swing wobble wiggle pulse flip flipInX flipOutX flipInY flipOutY fadeIn fadeInUp fadeInDown fadeInLeft fadeInRight fadeInUpBig fadeInDownBig fadeInLeftBig fadeInRightBig fadeOut fadeOutUp fadeOutDown fadeOutLeft fadeOutRight fadeOutUpBig fadeOutDownBig fadeOutLeftBig fadeOutRightBig bounceIn bounceInDown bounceInUp bounceInLeft bounceInRight bounceOut bounceOutDown bounceOutUp bounceOutLeft bounceOutRight rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight lightSpeedIn lightSpeedOut hinge rollIn rollOut';
   animationList = dummyStr.split(' ');
   element = document.getElementById('animate-me');
+  
+  element.addEventListener('click', function () {
+    testAnim(60);
+  }, false);
 
-  element.addEventListener("webkitAnimationStart", function () {
+  PrefixedEvent(element, "AnimationStart", function () {
     timerA = Date.now();
     console.log('animation start');
-    doLSOP();
-  }, false);
+    setTimeout(doLSOP, 1);
+  });
 
-  element.addEventListener("webkitAnimationEnd", function () {
-      // is this about a 1000ms???
-      console.log("animation " + randAnim + " has completed in " + (Date.now() - timerA) + "ms");
-      element.className = element.className.replace(' ' + randAnim, '');
-  }, false);
+  PrefixedEvent(element, "AnimationEnd", function () {
+    // is this about a 1000ms???
+    console.log("animation " + randAnim + " has completed in " + (Date.now() - timerA) + "ms");
+    element.className = element.className.replace(' ' + randAnim, '');
+  });
+  
   localStorage.clear();
 
   for (x = 3; x > 0; x--) { dummyStr += dummyStr; }
@@ -61,6 +75,4 @@
     }
     console.log("ls op during " + randAnim + " has completed in " + (Date.now() - timerLS) + "ms");
   }
-
-  testAnim(100);
 }).call(this);
