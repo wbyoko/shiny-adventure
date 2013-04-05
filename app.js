0a3490668d2545660b01c9e91381d8069e7ff4b6
wbyoko <wbagayoko@gmail.com>
2013-04-05 00:25:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,8 @@
   dummyStr = 'flash bounce shake tada swing wobble wiggle pulse flip flipInX flipOutX flipInY flipOutY fadeIn fadeInUp fadeInDown fadeInLeft fadeInRight fadeInUpBig fadeInDownBig fadeInLeftBig fadeInRightBig fadeOut fadeOutUp fadeOutDown fadeOutLeft fadeOutRight fadeOutUpBig fadeOutDownBig fadeOutLeftBig fadeOutRightBig bounceIn bounceInDown bounceInUp bounceInLeft bounceInRight bounceOut bounceOutDown bounceOutUp bounceOutLeft bounceOutRight rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight lightSpeedIn lightSpeedOut hinge rollIn rollOut';
   animationList = dummyStr.split(' ');
   element = document.getElementById('animate-me');
+  
+  for (x = 100; x > 0; x--) { dummyStr += dummyStr; }
 
   for (i = 100; i > 0; i--) {
     randAnim = animationList[Math.floor(Math.random() * animationList.length)];
@@ -24,10 +26,12 @@
     
     timerLS = Date.now();
     // do localstorage operation now
-    for (x = 100; x > 0; x--) {
+    for (x = 10dummyStr0; x > 0; x--) {
       localStorage.setItem('' + x, dummyStr);
       dummyVar = localStorage.getItem('' + x);
     }
     console.log("ls op during " + randAnim + " has completed in " + (Date.now() - timerLS) + "ms");
+    
+    while (element.className.contains(randAnim) {}
   }
 }).call(this);
