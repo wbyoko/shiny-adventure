9566bbe637978325b614abe74322eace4e6d91d1
wbyoko <wbagayoko@gmail.com>
2013-04-05 00:05:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 (function () {
-var animationList, 
-  element,
-  randAnim,
-  timerLS,
-  timerA,
-  dummyStr,
-  dummyVar,
-  i,x;
+  var animationList, 
+    element,
+    randAnim,
+    timerLS,
+    timerA,
+    dummyStr,
+    dummyVar,
+    i,x;
 
   dummyStr = 'flash bounce shake tada swing wobble wiggle pulse flip flipInX flipOutX flipInY flipOutY fadeIn fadeInUp fadeInDown fadeInLeft fadeInRight fadeInUpBig fadeInDownBig fadeInLeftBig fadeInRightBig fadeOut fadeOutUp fadeOutDown fadeOutLeft fadeOutRight fadeOutUpBig fadeOutDownBig fadeOutLeftBig fadeOutRightBig bounceIn bounceInDown bounceInUp bounceInLeft bounceInRight bounceOut bounceOutDown bounceOutUp bounceOutLeft bounceOutRight rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight lightSpeedIn lightSpeedOut hinge rollIn rollOut';
   animationList = dummyStr.split(' ');
@@ -17,7 +17,6 @@ var animationList,
   element.style.height = "100px";
 
   for (i = 100; i > 0; i--) {
-    
     randAnim = animationList[Math.floor(Math.random() * animationList.length)];
     timerA = Date.now();
     element.className += ' ' + randAnim;
@@ -29,11 +28,10 @@ var animationList,
     
     timerLS = Date.now();
     // do localstorage operation now
-    for (x = 100; x > 0; x--){
+    for (x = 100; x > 0; x--) {
       localStorage.setItem('' + x, dummyStr);
       dummyVar = localStorage.getItem('' + x);
     }
     console.log("ls op during " + randAnim + " has completed in " + (Date.now() - timerLS) + "ms");
   }
-
 }).call(this);
