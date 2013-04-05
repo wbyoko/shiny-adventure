8f5607a1e23c6b760388998d331651baefd55903
wbyoko <wbagayoko@gmail.com>
2013-04-05 01:39:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,36 +6,61 @@
     timerA,
     dummyStr,
     dummyVar,
+    interval,
     i, x, s;
 
   dummyStr = 'flash bounce shake tada swing wobble wiggle pulse flip flipInX flipOutX flipInY flipOutY fadeIn fadeInUp fadeInDown fadeInLeft fadeInRight fadeInUpBig fadeInDownBig fadeInLeftBig fadeInRightBig fadeOut fadeOutUp fadeOutDown fadeOutLeft fadeOutRight fadeOutUpBig fadeOutDownBig fadeOutLeftBig fadeOutRightBig bounceIn bounceInDown bounceInUp bounceInLeft bounceInRight bounceOut bounceOutDown bounceOutUp bounceOutLeft bounceOutRight rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight lightSpeedIn lightSpeedOut hinge rollIn rollOut';
   animationList = dummyStr.split(' ');
   element = document.getElementById('animate-me');
-  localStorage.clear();
-  
-  for (x = 5; x > 0; x--) { dummyStr += dummyStr; }
 
-  for (i = 100; i > 0; i--) {
-    randAnim = animationList[Math.floor(Math.random() * animationList.length)];
+  element.addEventListener("webkitAnimationStart", function () {
     timerA = Date.now();
-    element.className += ' ' + randAnim;
-    setTimeout(function () {
+    console.log('animation start');
+    doLSOP();
+  }, false);
+
+  element.addEventListener("webkitAnimationEnd", function () {
       // is this about a 1000ms???
       console.log("animation " + randAnim + " has completed in " + (Date.now() - timerA) + "ms");
       element.className = element.className.replace(' ' + randAnim, '');
-    }, 1000);
+  }, false);
+  localStorage.clear();
 
+  for (x = 3; x > 0; x--) { dummyStr += dummyStr; }
+
+  function testAnim (countLeft) {
+    if (countLeft <= 0) {
+      console.log("v 1.0.3");
+      return;
+    }
+    randAnim = animationList[Math.floor(Math.random() * animationList.length)];
+    //timerA = Date.now();
+    element.className += ' ' + randAnim;
+
+    interval = setInterval(function() {
+      if (element.className.indexOf(randAnim) == -1) {
+        clearInterval(interval);
+        setTimeout(function () {
+          console.log('next animation');
+          testAnim(countLeft - 1);
+        }, 1000);
+      }
+    }, 100);
+  }
+
+  function doLSOP () {
     timerLS = Date.now();
+    console.log('ls op started ' + (Date.now() - timerA) + "ms later");
+
     // do localstorage operation now
-    for (s = 200; s > 0; x--) {
-      for (x = 1; x > 0; x--) {
+    for (s = 100; s > 0; s--) {
+      for (x = 200; x > 0; x--) {
         localStorage.setItem('' + x, dummyStr);
         dummyVar = localStorage.getItem('' + x);
       }
     }
     console.log("ls op during " + randAnim + " has completed in " + (Date.now() - timerLS) + "ms");
-
-    //while (element.className.contains(randAnim)) {}
   }
-  console.log("v 1.0.1");
+
+  testAnim(100);
 }).call(this);
